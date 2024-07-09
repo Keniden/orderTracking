2c55296344429da40143a7e68d6c29cfc9723357
Keniden <nik.glumov.94@mail.ru>
2024-07-09 23:20:13 +0300

    fix arh

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"context"
-	"net/http"
+	"orderTracking/internal/api"
 	"orderTracking/internal/configs"
 	"orderTracking/internal/handler"
-	"time"
 
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
@@ -22,30 +20,11 @@ import (
 //@in Header
 //@name Authorization
 
-type Server struct {
-	httpServer *http.Server
-}
-
-func (s *Server) Run(port string, handler http.Handler) error {
-	s.httpServer = &http.Server{
-		Addr:           ":" + port,
-		Handler:        handler,
-		MaxHeaderBytes: 1 << 20, // 1 mb
-		ReadTimeout:    10 * time.Second,
-		IdleTimeout:    10 * time.Second,
-	}
-	return s.httpServer.ListenAndServe()
-}
-
-func (s *Server) Shutdown(ctx context.Context) error {
-	return s.httpServer.Shutdown(ctx)
-}
-
 func main() {
 	logrus.SetFormatter(new(logrus.JSONFormatter))
 	db := configs.InitConfig()
 	handlers := handler.InitApp(db)
-	server := new(Server)
+	server := new(api.Server)
 	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
 		logrus.Fatal("error occured while running http server: %s", err.Error())
 	}
